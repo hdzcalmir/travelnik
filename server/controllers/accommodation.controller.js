d2ce0fd6a04d5a655aa1df083ae8359701672c6c
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2023-10-29 18:47:12 +0100

    Documentation for accommodations API

--- server/controllers/accommodation.controller.js
+++ server/controllers/accommodation.controller.js
@@ -5,68 +5,27 @@ import { logger } from "../utils/logger.js";
 
 /**
  * @swagger
- * components:
- *   schemas:
- *     Accommodation:
- *       type: object
- *       properties:
- *         id:
- *           type: number
- *           description: The auto-generated id of the accommodation
- *         title:
- *           type: string
- *           description: The title of accommodation
- *         source:
- *           type: string
- *           description: The accommodation booking source URL
- *         image:
- *           type: string
- *           description: Thumbnail image of accommodation
- *         rating:
- *           type: number
- *           description: Rating from booking.com
- *         price:
- *           type: number
- *           description: Price of accommodation
- *         distance_from_center:
- *           type: string
- *           description: How far away accommodation is from center
- *         reviews:
- *           type: number
- *           description: Number of reviews left on booking.com
- *         status:
- *           type: string
- *           description: One keyword status of accommodation status
- *         longitude:
- *           type: number
- *           description: Longitude of accommodation
- *         latitude:
- *           type: number
- *           description: Latitude of accommodation
- *         check_in_time:
- *           type: string
- *           description: Time for checking in
- *         check_out_time:
- *           type: string
- *           description: Time for checking out
- *         address:
- *           type: string
- *           description: Address of accommodation
- *       example:
- *         id: 1
- *         title: Luxury Hotel
- *         source: https://www.example.com/hotel
- *         image: https://www.example.com/images/hotel.jpg
- *         rating: 9.2
- *         price: 150
- *         distance_from_center: 1.5 miles
- *         reviews: 120
- *         status: Available
- *         longitude: 17.123456
- *         latitude: 44.654321
- *         check_in_time: 15:00
- *         check_out_time: 11:00
- *         address: 123 Main Street
+ * /api/accommodation:
+ *   get:
+ *     tags:
+ *       - Accommodations
+ *     summary: Get All Accommodations
+ *     description: Retrieve a list of all accommodations.
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Accommodation'
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Internal server error.
  */
 export const getAllAccomodation = (req, res) => {
   try {
@@ -78,6 +37,29 @@ export const getAllAccomodation = (req, res) => {
     return res.status(500).send("Internal server error.");
   }
 };
+
+/**
+ * @swagger
+ * /api/accommodation:
+ *   patch:
+ *     tags:
+ *       - Accommodations
+ *     summary: Update Accommodations from Booking API
+ *     description: Retrieve and update accommodations from the Booking API.
+ *     responses:
+ *       '200':
+ *         description: Successful update
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Accommodation successfully updated.
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Internal server error.
+ */
 
 export const updateAccommodation = async (req, res) => {
   try {
--- server/routes/accommodation.routes.js
+++ server/routes/accommodation.routes.js
@@ -6,6 +6,72 @@ import express from "express";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Accommodation:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: number
+ *           description: The auto-generated id of the accommodation
+ *         title:
+ *           type: string
+ *           description: The title of accommodation
+ *         source:
+ *           type: string
+ *           description: The accommodation booking source URL
+ *         image:
+ *           type: string
+ *           description: Thumbnail image of accommodation
+ *         rating:
+ *           type: number
+ *           description: Rating from booking.com
+ *         price:
+ *           type: number
+ *           description: Price of accommodation
+ *         distance_from_center:
+ *           type: string
+ *           description: How far away accommodation is from center
+ *         reviews:
+ *           type: number
+ *           description: Number of reviews left on booking.com
+ *         status:
+ *           type: string
+ *           description: One keyword status of accommodation status
+ *         longitude:
+ *           type: number
+ *           description: Longitude of accommodation
+ *         latitude:
+ *           type: number
+ *           description: Latitude of accommodation
+ *         check_in_time:
+ *           type: string
+ *           description: Time for checking in
+ *         check_out_time:
+ *           type: string
+ *           description: Time for checking out
+ *         address:
+ *           type: string
+ *           description: Address of accommodation
+ *       example:
+ *         id: 1
+ *         title: Luxury Hotel
+ *         source: https://www.example.com/hotel
+ *         image: https://www.example.com/images/hotel.jpg
+ *         rating: 9.2
+ *         price: 150
+ *         distance_from_center: 1.5 miles
+ *         reviews: 120
+ *         status: Available
+ *         longitude: 17.123456
+ *         latitude: 44.654321
+ *         check_in_time: 15:00
+ *         check_out_time: 11:00
+ *         address: 123 Main Street
+ */
+
 router.get("/", getAllAccomodation);
 router.patch("/", updateAccommodation);
 
