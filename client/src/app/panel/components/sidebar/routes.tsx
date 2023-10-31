63e2f8cf646538efa942a4d6d68fa1e6c0dd5e3c
Almir Hodžić <81625008+hdzcalmir@users.noreply.github.com>
2023-10-31 20:47:20 +0100

    Progress

---
+++ client/src/app/panel/components/sidebar/routes.tsx
@@ -0,0 +1,18 @@
+export const routes = [
+    {
+        dashboard: 'Dashboard',
+        route: '/panel/dashboard'
+    },
+    {
+        dashboard: 'Add Location',
+        route: '/panel/addlocation'
+    },
+    {
+        dashboard: 'Add Event',
+        route: '/panel/addevent'
+    },
+    {
+        dashboard: 'Add Activity',
+        route: '/panel/addactivity'
+    }
+]
--- client/src/app/panel/dashboard/page.tsx
+++ client/src/app/panel/dashboard/page.tsx
@@ -6,11 +6,34 @@ export default function Dashboard() {
   return (
     <div className="flex-row lg:flex">
       <Sidebar></Sidebar>
-      <div className="container mx-auto mt-4 lg:mt-12">
-        <div className="p-4 mx-2 shadow-sm">
-          <p>Add Dashboard Analytics </p>
-        </div>
-      </div>
+      <div className="container mx-auto mt-12">
+                <div className="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-3">
+                    <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
+                        <div className="text-sm font-medium text-gray-500 truncate">
+                            Ukupno lokacija
+                        </div>
+                        <div className="mt-1 text-3xl font-semibold text-gray-900">
+                            125
+                        </div>
+                    </div>
+                    <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
+                        <div className="text-sm font-medium text-gray-500 truncate">
+                            Ukupno aktivnosti
+                        </div>
+                        <div className="mt-1 text-3xl font-semibold text-gray-900">
+                            1259
+                        </div>
+                    </div>
+                    <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
+                        <div className="text-sm font-medium text-gray-500 truncate">
+                            Ukupno događaja
+                        </div>
+                        <div className="mt-1 text-3xl font-semibold text-gray-900">
+                           35
+                        </div>
+                    </div>
+                </div>
+            </div>
       </div>
   )
 }
