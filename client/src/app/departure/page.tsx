e2e194c1c43aab34173a06aef210f551fb0b9519
Ibrahim Okić <ibrahimokic711@gmail.com>
2023-11-07 22:30:21 +0100

    Update page.tsx

--- client/src/app/departure/page.tsx
+++ client/src/app/departure/page.tsx
@@ -11,10 +11,9 @@ import BusinessAPI from "@/interceptor/Business/Business";
 import EventAPI from "@/interceptor/Event/Event";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import mapboxgl, { Marker } from 'mapbox-gl';
-import { GEO_LOC, LOAD, MAP, TOKEN } from '@/common/consts';
 import ActivityTable from "@/components/home/departure/ActivityTable";
 import EventCalendar from "@/components/home/departure/EventCalendar/EventCalendar";
+import Map from "@/components/map/map";
 
 const AboutPage = () => {
     // >> React Hooks
@@ -38,82 +37,6 @@ const AboutPage = () => {
 
 
     useEffect(() => {
-        const ventures: Array<any> = [
-            {
-                "lng": 17.65746447619486,
-                "lat": 44.20660338455724
-            },
-            {
-                "lng": 17.64364880427837,
-                "lat": 44.22899848897313
-            },
-            {
-                "lng": 17.67395832304308,
-                "lat": 44.22377053265586
-            },
-            {
-                "lng": 17.663315317671504,
-                "lat": 44.22949050808299
-            },
-            {
-                "lng": 17.66280033354164,
-                "lat": 44.22131015722667
-            },
-            {
-                "lng": 17.652414820234895,
-                "lat": 44.22161770977837
-            },
-            {
-                "lng": 17.653101465742736,
-                "lat": 44.22758391136591
-            },
-            {
-                "lng": 17.623489878218948,
-                "lat": 44.223955056668046
-            },
-            {
-                "lng": 17.620829126876004,
-                "lat": 44.234594962996255
-            },
-            {
-                "lng": 17.637909433882783,
-                "lat": 44.231766042673655
-            }
-        ];
-        if (interests?.length === 0 || check_in?.length === 0 || check_out?.length === 0 || people?.length === 0) {
-            router.push('/');
-        }
-
-        // >> Mapbox
-        mapboxgl.accessToken = TOKEN;
-        const map = new mapboxgl.Map({
-            container: MAP,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [GEO_LOC[0], GEO_LOC[1]],
-            zoom: 13
-        });
-
-        const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-            'Dobrodošli u Travnik!'
-        );
-
-        map.on('click', (data) => {
-            const venture = new mapboxgl.Marker({
-            }).setLngLat(data.lngLat)
-                .setPopup(popup)
-                .addTo(map);
-        })
-
-        map.on(LOAD, () => {
-            ventures.forEach((addr) => {
-                let el = document.createElement('div');
-                el.className = 'marker';
-                new mapboxgl.Marker(el)
-                    .setLngLat(addr)
-                    .setPopup(popup)
-                    .addTo(map);
-            })
-        })
 
         // >> Fetch relevant data from back-end API
         const fetchBusinesses = async () => {
@@ -141,9 +64,7 @@ const AboutPage = () => {
             <Navbar />
             <div className="grid lg:grid-cols-3 grid-rows-1">
                 <div className="col-span-2">
-                    <div className="flex justify-center">
-                        <div className="h-[680px] w-full rounded-lg" id="map"></div>
-                    </div>
+                    <div className="h-[680px] w-full rounded-lg" ><Map /></div>
                     <ActivityTable activities={activities} />
                 </div>
                 <div className="pb-4 xs:w-full bg-gray-800">
