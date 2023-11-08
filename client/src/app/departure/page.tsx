d12a606a3f038efbaf8f3affbbfbec070bf3327c
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2023-11-08 11:05:31 +0100

    Responsive fix on departure page

--- client/src/app/departure/page.tsx
+++ client/src/app/departure/page.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
 import { IActivity } from "@/common/interfaces/IActivity";
 import { IEvent } from "@/common/interfaces/IEvent";
 import { IVenture } from "@/common/interfaces/IVenture";
 import AccommodationList from "@/components/home/accommodation/AccommodationList";
-import Navbar from "@/components/home/navbar/Navbar"
+import Navbar from "@/components/home/navbar/Navbar";
 import useAccommodations from "@/hooks/useAccommodations";
 import ActivityAPI from "@/interceptor/Activity/Activity";
 import BusinessAPI from "@/interceptor/Business/Business";
@@ -16,69 +16,93 @@ import EventCalendar from "@/components/home/departure/EventCalendar/EventCalend
 import Map from "@/components/map/map";
 
 const AboutPage = () => {
-    // >> React Hooks
-    const params = useSearchParams();
-    const router = useRouter();
+  // >> React Hooks
+  const params = useSearchParams();
+  const router = useRouter();
 
-    // >> States & Hooks
-    const { accommodations, accommodationsLoading } = useAccommodations();
-    const [businesses, setBusinesses] = useState<IVenture[]>();
-    const [activities, setActivities] = useState<IActivity[]>();
-    const [events, setEvents] = useState<IEvent[]>();
+  // >> States & Hooks
+  const { accommodations, accommodationsLoading } = useAccommodations();
+  const [businesses, setBusinesses] = useState<IVenture[]>();
+  const [activities, setActivities] = useState<IActivity[]>();
+  const [events, setEvents] = useState<IEvent[]>();
 
-    // >> Filters
-    const interests = params.get('interests');
-    const check_in = params.get('check_in');
-    const check_out = params.get('check_out');
-    const people = params.get('people');
+  // >> Filters
+  const interests = params.get("interests");
+  const check_in = params.get("check_in");
+  const check_out = params.get("check_out");
+  const people = params.get("people");
 
-    const startDate = check_in ? new Date(check_in) : undefined;
-    const endDate = check_out ? new Date(check_out) : undefined;
+  const startDate = check_in ? new Date(check_in) : undefined;
+  const endDate = check_out ? new Date(check_out) : undefined;
 
+  useEffect(() => {
+    // >> Fetch relevant data from back-end API
+    const fetchBusinesses = async () => {
+      const response = await BusinessAPI.fetchBusinessesWithFilters(
+        interests,
+        check_in,
+        check_out,
+        people
+      );
+      setBusinesses(response.data);
+    };
 
-    useEffect(() => {
+    const fetchActivities = async () => {
+      const response = await ActivityAPI.fetchActivitiesWithFilters(
+        interests,
+        check_in,
+        check_out,
+        people
+      );
+      setActivities(response.data);
+    };
 
-        // >> Fetch relevant data from back-end API
-        const fetchBusinesses = async () => {
-            const response = await BusinessAPI.fetchBusinessesWithFilters(interests, check_in, check_out, people);
-            setBusinesses(response.data);
-        }
+    const fetchEvents = async () => {
+      const response = await EventAPI.fetchEventsWithFilters(
+        interests,
+        check_in,
+        check_out,
+        people
+      );
+      setEvents(response.data);
+    };
 
-        const fetchActivities = async () => {
-            const response = await ActivityAPI.fetchActivitiesWithFilters(interests, check_in, check_out, people);
-            setActivities(response.data);
-        }
-
-        const fetchEvents = async () => {
-            const response = await EventAPI.fetchEventsWithFilters(interests, check_in, check_out, people);
-            setEvents(response.data);
-        }
-
-        fetchBusinesses();
-        fetchActivities();
-        fetchEvents();
-    }, [router, interests, check_in, check_out, people])
-    console.log(events)
-    return (
-        <>
-            <Navbar />
-            <div className="grid lg:grid-cols-3 grid-rows-1">
-                <div className="col-span-2">
-                    <div className="h-[680px] w-full rounded-lg" ><Map /></div>
-                    <ActivityTable activities={activities} />
-                </div>
-                <div className="pb-4 xs:w-full bg-gray-800">
-                    <EventCalendar startDate={startDate} endDate={endDate} events={events} />
-                    <div className="mx-2 mt-4">
-                        <h2 className="text-3xl font-bold mx-2 text-white mb-2">Available apartments</h2>
-                        <div className="overflow-y-auto h-[56.4vh] px-2 scrollbar-hidden">
-                            <AccommodationList accommodations={accommodations} accommodationsLoading={accommodationsLoading} />
-                        </div>
-                    </div>
-                </div>
+    fetchBusinesses();
+    fetchActivities();
+    fetchEvents();
+  }, [router, interests, check_in, check_out, people]);
+  console.log(events);
+  return (
+    <>
+      <Navbar />
+      <div className="grid xl:grid-cols-3 grid-rows-1 grid-cols-1">
+        <div className="col-span-2">
+          <div className="h-[680px] w-full rounded-lg">
+            <Map />
+          </div>
+          <ActivityTable activities={activities} />
+        </div>
+        <div className="pb-4 xs:w-full bg-gray-800 pt-4 xl:pt-0">
+          <EventCalendar
+            startDate={startDate}
+            endDate={endDate}
+            events={events}
+          />
+          <div className="mx-2 mt-4">
+            <h2 className="text-3xl font-bold mx-2 text-white mb-2">
+              Available apartments
+            </h2>
+            <div className="overflow-y-auto h-[56.4vh] px-2 scrollbar-hidden">
+              <AccommodationList
+                accommodations={accommodations}
+                accommodationsLoading={accommodationsLoading}
+              />
             </div>
-        </>
-    );
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default AboutPage;
--- client/src/components/home/accommodation/AccommodationCard/AccommodationCard.tsx
+++ client/src/components/home/accommodation/AccommodationCard/AccommodationCard.tsx
@@ -1,62 +1,89 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
 
 export interface IAccommodation {
-    id: number;
-    source: string;
-    image: string;
-    title: string;
-    rating: number;
-    price: number;
-    distance_from_center: number;
-    reviews: number;
-    status: string;
-    longitude: number;
-    latitude: number;
-    check_in_time: string;
-    check_out_time: string;
-    address: string;
+  id: number;
+  source: string;
+  image: string;
+  title: string;
+  rating: number;
+  price: number;
+  distance_from_center: number;
+  reviews: number;
+  status: string;
+  longitude: number;
+  latitude: number;
+  check_in_time: string;
+  check_out_time: string;
+  address: string;
 }
 
 const AccommodationCard = (data: IAccommodation) => {
-    return (
-        <div className="md:max-w-2xl max-w-xs">
-            <Link href={data.source} target="_blank" className="space-y-10">
-                <div
-                    className="mb-4 cursor-pointer relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-2xl bg-gray-700">
-                    <div className="w-full md:w-1/3 bg-gray-700 grid place-items-center text-gray-800">
-                        <Image src={data.image.replace('square60', 'square600')} alt={data.title} className="rounded-xl" width={400} height={400} loading="lazy" />
-                    </div>
-                    <div className="w-full md:w-2/3 bg-gray-700 flex flex-col space-y-2 p-3">
-                        <div className="flex justify-between item-center">
-                            <p className="text-gray-300 font-medium md:block">{data.status ? data.status : "No reviews"}</p>
-                            <div className="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-500" viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path
-                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                                </svg>
-                                <p className="text-white font-bold text-sm ml-1">
-                                    {data.rating}
-                                    <span className="text-gray-300 font-normal">({data.reviews} reviews)</span>
-                                </p>
-                            </div>
-                            <div className="bg-gray-500 px-3 py-1 rounded-full text-xs font-medium text-white hidden md:block">
-                                Booking.com</div>
-                        </div>
-                        <h3 className="font-bold text-white md:text-3xl text-xl">{data.title}</h3>
-                        <p className="md:text-md text-gray-300 text-base"><span className="text-white font-bold">Address:</span> {data.address} <br></br> <span className="text-white font-bold">Distance from center:</span> {data.distance_from_center}km <br></br> <span className="text-white font-bold">Check-in time:</span> {data.check_in_time}h | <span className="text-white font-bold">Check-out time:</span> {data.check_out_time}h</p>
-                        <p className="text-xl font-black text-white">
-                            €{data.price}
-                            <span className="font-normal text-gray-300 text-base">/day</span>
-                        </p>
-                    </div>
-                </div>
-            </Link>
+  return (
+    <div className="md:max-w-2xl max-w-xs">
+      <Link href={data.source} target="_blank" className="space-y-10">
+        <div className="mb-4 cursor-pointer relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-2xl bg-gray-700">
+          <div className="w-full md:w-1/3 bg-gray-700 grid place-items-center text-gray-800">
+            <Image
+              src={data.image.replace("square60", "square600")}
+              alt={data.title}
+              className="rounded-xl"
+              width={400}
+              height={400}
+              loading="lazy"
+            />
+          </div>
+          <div className="w-full md:w-2/3 bg-gray-700 flex flex-col space-y-2 p-3">
+            <div className="flex justify-between item-center">
+              <p className="text-gray-300 font-medium md:block">
+                {data.status ? data.status : "No reviews"}
+              </p>
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-500"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <p className="text-white font-bold text-sm ml-1">
+                  {data.rating}
+                  <span className="text-gray-300 font-normal">
+                    ({data.reviews} reviews)
+                  </span>
+                </p>
+              </div>
+              <div className="bg-gray-500 px-3 py-1 rounded-full text-xs font-medium text-white hidden md:block">
+                Booking.com
+              </div>
+            </div>
+            <h3 className="font-bold text-white md:text-3xl text-xl">
+              {data.title}
+            </h3>
+            <p className="md:text-md text-gray-300 text-base">
+              <span className="text-white font-bold">Address:</span>{" "}
+              {data.address} <br></br>{" "}
+              <span className="text-white font-bold">
+                Distance from center:
+              </span>{" "}
+              {data.distance_from_center}km <br></br>{" "}
+              <span className="text-white font-bold">Check-in time:</span>{" "}
+              {data.check_in_time}h |{" "}
+              <span className="text-white font-bold">Check-out time:</span>{" "}
+              {data.check_out_time}h
+            </p>
+            <p className="text-xl font-black text-white">
+              €{data.price}
+              <span className="font-normal text-gray-300 text-base">/day</span>
+            </p>
+          </div>
         </div>
-    );
-}
+      </Link>
+    </div>
+  );
+};
 
 export default AccommodationCard;
