a5862854361143d82555f026808c3d6e5182c535
Ibrahim Okić <ibrahimokic711@gmail.com>
2023-11-09 21:52:43 +0100

    Update ActivityFilter.tsx

--- client/src/components/home/departure/ActivityCard/ActivityFilter.tsx
+++ client/src/components/home/departure/ActivityCard/ActivityFilter.tsx
@@ -51,7 +51,7 @@ const ActivityFilter = ({
               onChange={(e: ChangeEvent<HTMLInputElement>) =>
                 setSearchFilter(e.target.value)
               }
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="bg-gray-50 border outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
               placeholder="Search"
             />
           </div>
