732c782fabd2dc0380f05712b83b5db3306d202b
Ibrahim Okić <ibrahimokic711@gmail.com>
2023-11-10 15:11:43 +0100

    Update Language.tsx

--- client/src/components/home/navbar/Language/Language.tsx
+++ client/src/components/home/navbar/Language/Language.tsx
@@ -7,13 +7,12 @@ import { useLocale, useTranslations } from "next-intl";
 import Link from "next-intl/link";
 
 const Language = () => {
-    const [language, setLanguage] = useState<string>("English");
     const [languageDropdown, setLanguageDropDown] = useState<boolean>(false);
     const languageRef = useRef<HTMLDivElement>(null);
     const t = useTranslations('Languages');
     const locale = useLocale();
 
-    const currentLanguage = Languages.find(lang => lang.name === language);
+    const currentLanguage = Languages.find(lang => lang.locale === locale);
 
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
@@ -28,10 +27,6 @@ const Language = () => {
             document.removeEventListener('click', handleClickOutside);
         };
     }, []);
-
-    const handleSetLanguage = (language: any) => {
-        setLanguage(language.name)
-    }
 
     return (
         <div className="relative" ref={languageRef}>
@@ -62,7 +57,7 @@ const Language = () => {
             <div id="dropdownDelay" className={`z-10 ${languageDropdown ? "absolute " : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow-lg w-44 dark:bg-gray-700`}>
                 <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDelayButton">
                     {Languages && Languages.map((language) => (
-                        <Link onClick={() => handleSetLanguage(language)} href="" locale={language.locale} key={language.name} className="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer">
+                        <Link href="/" locale={language.locale} key={language.name} className="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer">
                             <Image src={language.icon} width={20} height={20} alt={`Flag of ${language.name}`} className="mr-2" />
                             {t(language.name)}
                         </Link>
