7917987118686769d99de8984ae0932314a74cdf
Ibrahim Okić <ibrahimokic711@gmail.com>
2023-11-10 16:06:16 +0100

    AI Generated images

--- client/src/app/[locale]/about/page.tsx
+++ client/src/app/[locale]/about/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import Footer from "@/components/home/footer/Footer";
 import Navbar from "@/components/home/navbar/Navbar"
 
 const AboutPage = () => {
     return (
         <>
             <Navbar />
+            <Footer />
         </>
     );
 }
