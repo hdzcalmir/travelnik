a36add98bd2085147bdf4213760f61fa0b6455b7
Almir Hodžić <81625008+hdzcalmir@users.noreply.github.com>
2023-11-11 10:39:22 +0100

    Tables ui update

--- client/src/app/[locale]/panel/events/page.tsx
+++ client/src/app/[locale]/panel/events/page.tsx
@@ -36,12 +36,12 @@ function Events() {
             <div className="p-2 sm:p-4 sm:ml-64 h-full bg-gray-700">
                 <Breadcrumb homeElement={'Home'}></Breadcrumb>
                 <div className="flex flex-col w-full items-center border-gray-200 h-[80vh] dark:border-gray-700">
-                    <div className="flex flex-col shadow-lg items-center w-full h-full mb-4 rounded-lg bg-gray-600">
-                        <div className="flex h-10 mt-5 border-b border-gray-700 w-full px-5">
-                            <h2 className="text-gray-50 font-semibold text-xl">Events</h2>
+                    <div className="flex flex-col shadow-lg items-center w-full h-full mb-4 rounded-lg bg-gray-800">
+                        <div className="flex h-10 mt-5  w-full px-5">
+                            <h2 className="text-gray-50 font-bold text-xl">Events</h2>
                         </div>
-                        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-600 dark:text-gray-300">
+                        <table className="w-full text-sm text-left rtl:text-right  dark:text-gray-400">
+                            <thead className="text-xs border-b border-gray-700 uppercase bg-gray-800 text-gray-100">
                                 <tr>
                                     <th scope="col" className="px-6 py-3">
                                         Name
@@ -65,8 +65,8 @@ function Events() {
                             </thead>
                             <tbody>
                                 {events?.map((activity) => (
-                                    <tr key={activity.id} className="dark:bg-gray-800 border-b dark:border-gray-700">
-                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <tr key={activity.id} className="bg-gray-800 border-b border-gray-700">
+                                        <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap">
                                             {activity.name}
                                         </th>
                                         <td className="px-6 py-4">
