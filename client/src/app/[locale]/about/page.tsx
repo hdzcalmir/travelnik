f2d47497d1285ac546ac5900c32603cdffeab6ab
Ibrahim Okić <ibrahimokic711@gmail.com>
2023-11-11 21:12:17 +0100

    Update page.tsx

--- client/src/app/[locale]/about/page.tsx
+++ client/src/app/[locale]/about/page.tsx
@@ -99,7 +99,7 @@ const AboutPage = () => {
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-col text-center w-full mb-12">
                         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Contact Us</h1>
-                        <p className="lg:w-2/3 mx-auto leading-relaxed text-gray-400">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-gray-400">Interested in collaborating with Travelnik? Reach out to discuss potential partnerships and collaborations.</p>
                     </div>
                     <div className="lg:w-1/2 md:w-2/3 mx-auto">
                         <div className="flex flex-wrap -m-2">
