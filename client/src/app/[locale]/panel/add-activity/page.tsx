d35b0b4ec7894fce0905479a6fe133213d72b87d
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2023-11-11 13:38:30 +0100

    Imports fixed for sidebar and footer on panel

--- client/src/app/[locale]/panel/add-activity/page.tsx
+++ client/src/app/[locale]/panel/add-activity/page.tsx
@@ -1,7 +1,19 @@
-"use client"
-import { ADDRESS, CITY, CLICK, COUNTRY, COORDS, LAT, LNG, MAP, POST_CODE, TOKEN, DEFAULT_MARKER } from "@/common/consts";
-import Footer from "@/components/panel/layout/footer/Footer";
-import Sidebar from "@/components/panel/layout/sidebar/Sidebar";
+"use client";
+import {
+  ADDRESS,
+  CITY,
+  CLICK,
+  COUNTRY,
+  COORDS,
+  LAT,
+  LNG,
+  MAP,
+  POST_CODE,
+  TOKEN,
+  DEFAULT_MARKER,
+} from "@/common/consts";
+import Sidebar from "@/components/panel/layout/sidebar/sidebar";
+import Footer from "@/components/panel/layout/footer/footer";
 import IsAuth from "@/hooks/isAuth";
 import ActivityAPI from "@/interceptor/Activity/Activity";
 import { mapboxApi } from "@/interceptor/mapboxApi";
@@ -12,161 +24,246 @@ import { interests } from "@/common/interests";
 import { difficulties } from "@/common/difficulties";
 
 const addActivity = async (activity: any) => {
-    await ActivityAPI.addActivtiy(activity);
-}
+  await ActivityAPI.addActivtiy(activity);
+};
 
 function AddActivity() {
+  let markerExists = false;
+  let marker: Marker;
 
-    let markerExists = false;
-    let marker: Marker;
+  const [activity, setActivity] = useState({
+    name: "",
+    category: "",
+    longitude: 0,
+    latitude: 0,
+    address: "",
+    city: "",
+    country: "",
+    postalCode: "",
+    description: "",
+    difficulty: "",
+    duration: "",
+  });
 
-    const [activity, setActivity] = useState({
-        name: '',
-        category: '',
-        longitude: 0,
-        latitude: 0,
-        address: '',
-        city: '',
-        country: '',
-        postalCode: '',
-        description: '',
-        difficulty: '',
-        duration: ''
+  useEffect(() => {
+    mapboxgl.accessToken = TOKEN;
+    const map = new mapboxgl.Map({
+      container: MAP,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [COORDS[0], COORDS[1]],
+      zoom: 14,
     });
 
-    useEffect(() => {
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, "top-left");
 
-        mapboxgl.accessToken = TOKEN;
-        const map = new mapboxgl.Map({
-            container: MAP,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [COORDS[0], COORDS[1]],
-            zoom: 14
-        });
+    map.on(CLICK, async (data) => {
+      if (!markerExists) {
+        let icon = document.createElement("div");
+        icon.className = DEFAULT_MARKER;
 
-        const nav = new mapboxgl.NavigationControl();
-        map.addControl(nav, 'top-left');
+        const resp = await mapboxApi.reverseGeocode(data.lngLat);
+        if (resp?.features[4]?.text) {
+          marker = new mapboxgl.Marker(icon).setLngLat(data.lngLat).addTo(map);
+          setActivity({
+            ...activity,
+            [COUNTRY]: resp.features[4].text,
+            [CITY]: resp.features[2].text,
+            [POST_CODE]: resp.features[1].text,
+            [ADDRESS]: resp.features[0].text,
+            [LAT]: data.lngLat.lat,
+            [LNG]: data.lngLat.lng,
+          });
+          markerExists = true;
+        } else {
+          setActivity({
+            ...activity,
+            [COUNTRY]: "",
+            [CITY]: "",
+            [POST_CODE]: "",
+            [ADDRESS]: "",
+            [LAT]: 0,
+            [LNG]: 0,
+          });
+        }
+      } else {
+        marker?.remove();
+        let icon = document.createElement("div");
+        icon.className = DEFAULT_MARKER;
 
-        map.on(CLICK, async (data) => {
-            if (!markerExists) {
-                let icon = document.createElement('div');
-                icon.className = DEFAULT_MARKER;
+        const resp = await mapboxApi.reverseGeocode(data.lngLat);
+        if (resp?.features[4]?.text) {
+          marker = new mapboxgl.Marker(icon).setLngLat(data.lngLat).addTo(map);
+          setActivity({
+            ...activity,
+            [COUNTRY]: resp.features[4].text,
+            [CITY]: resp.features[2].text,
+            [POST_CODE]: resp.features[1].text,
+            [ADDRESS]: resp.features[0].text,
+            [LAT]: data.lngLat.lat,
+            [LNG]: data.lngLat.lng,
+          });
+          markerExists = true;
+        } else {
+          setActivity({
+            ...activity,
+            [COUNTRY]: "",
+            [CITY]: "",
+            [POST_CODE]: "",
+            [ADDRESS]: "",
+            [LAT]: 0,
+            [LNG]: 0,
+          });
+        }
+      }
+    });
+  }, []);
 
-                const resp = await mapboxApi.reverseGeocode(data.lngLat);
-                if (resp?.features[4]?.text) {
-                    marker = new mapboxgl.Marker(icon)
-                        .setLngLat(data.lngLat)
-                        .addTo(map);
-                    setActivity({ ...activity, [COUNTRY]: resp.features[4].text, [CITY]: resp.features[2].text, [POST_CODE]: resp.features[1].text, [ADDRESS]: resp.features[0].text, [LAT]: data.lngLat.lat, [LNG]: data.lngLat.lng });
-                    markerExists = true;
-                } else {
-                    setActivity({ ...activity, [COUNTRY]: '', [CITY]: '', [POST_CODE]: '', [ADDRESS]: '', [LAT]: 0, [LNG]: 0 });
-                }
+  const handleInputChange = (e: any) => {
+    setActivity({ ...activity, [e.target.name]: e.target.value });
+  };
 
-            } else {
-                marker?.remove();
-                let icon = document.createElement('div');
-                icon.className = DEFAULT_MARKER;
+  const handleAddActivity = async (e: any) => {
+    e.preventDefault();
+    await addActivity(activity);
+  };
 
-                const resp = await mapboxApi.reverseGeocode(data.lngLat);
-                if (resp?.features[4]?.text) {
-                    marker = new mapboxgl.Marker(icon)
-                        .setLngLat(data.lngLat)
-                        .addTo(map);
-                    setActivity({ ...activity, [COUNTRY]: resp.features[4].text, [CITY]: resp.features[2].text, [POST_CODE]: resp.features[1].text, [ADDRESS]: resp.features[0].text, [LAT]: data.lngLat.lat, [LNG]: data.lngLat.lng });
-                    markerExists = true;
-                } else {
-                    setActivity({ ...activity, [COUNTRY]: '', [CITY]: '', [POST_CODE]: '', [ADDRESS]: '', [LAT]: 0, [LNG]: 0 });
-                }
-            }
-        })
-    }, [])
-
-
-    const handleInputChange = (e: any) => {
-        setActivity({ ...activity, [e.target.name]: e.target.value });
-    }
-
-    const handleAddActivity = async (e: any) => {
-        e.preventDefault();
-        await addActivity(activity);
-    };
-
-    return (
-        <div>
-            <Sidebar></Sidebar>
-            <div className="p-2 sm:p-4 sm:ml-64 h-full bg-panelBg">
-                <div className="flex flex-col w-full items-center border-gray-200 h-full dark:border-gray-700">
-                    <Breadcrumb homeElement={'Home'}></Breadcrumb>
-                    <div className="rounded-lg shadow-lg h-96 w-full lg:w-2/3 mb-4" id="map">
-                    </div>
-                    <div className="flex flex-col shadow-lg items-center justify-center w-full lg:w-2/3 h-full mb-4 rounded-lg bg-gray-800">
-                        <div className="flex justify-start h-16 items-center border-b border-gray-700 w-full px-5">
-                            <h2 className="text-gray-50 font-bold text-xl">Add Activity</h2>
-                        </div>
-                        <form onSubmit={handleAddActivity} className="w-full space-y-2 py-5">
-                            <div className="flex justify-between w-full items-center px-5">
-                                <label className="text-md text-gray-50">Name</label>
-                                <input name="name" value={activity.name} onChange={handleInputChange} className="appearance-none rounded-lg bg-gray-700 border-none w-2/3 text-gray-50 py-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Activity name" />
-                            </div>
-                            <div className="flex justify-between w-full items-center px-5">
-                                <label className="text-md text-gray-50">Description</label>
-                                <input name="description" value={activity.description} onChange={handleInputChange} className="appearance-none rounded-lg bg-gray-700 border-none w-2/3 text-gray-50 py-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Activity description" />
-                            </div>
-                            <div className="flex justify-between w-full items-center px-5">
-                                <label className="text-md text-gray-50">Category</label>
-                                <select className="outline-none text-gray-50 py-3 px-2 bg-gray-700 rounded-lg w-2/3" id="category" name="category" value={activity.category} onChange={handleInputChange}>
-                                    <option>Select Category..</option>
-                                    {interests.map((interest) => (
-                                        <option key={interest} value={interest}>
-                                            {interest}
-                                        </option>
-                                    ))}
-                                </select>
-                            </div>
-                            <div className="flex justify-between w-full items-center px-5">
-                                <label className="text-md text-gray-50">Country</label>
-                                <input value={activity.country} disabled className="appearance-none bg-gray-700 border-none rounded-lg w-2/3 text-gray-50 py-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Country" />
-                            </div>
-                            <div className="flex justify-between w-full items-center px-5">
-                                <label className="text-md text-gray-50">City</label>
-                                <input value={activity.city} disabled className="appearance-none bg-gray-700 border-none w-2/3 rounded-lg text-gray-50 px-2 py-3 leading-tight focus:outline-none" type="text" placeholder="City" />
-                            </div>
-                            <div className="flex justify-between w-full items-center px-5">
-                                <label className="text-md text-gray-50">Postal Code</label>
-                                <input value={activity.postalCode} disabled className="appearance-none bg-gray-700 border-none w-2/3 rounded-lg text-gray-50 py-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Postal Code" />
-                            </div>
-                            <div className="flex justify-between w-full items-center px-5">
-                                <label className="text-md text-gray-50">Street</label>
-                                <input value={activity.address} disabled className="appearance-none bg-gray-700 border-none w-2/3 rounded-lg text-gray-50 py-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Street" />
-                            </div>
-                            <div className="flex justify-between w-full items-center px-5">
-                                <label className="text-md text-gray-50">Difficulty</label>
-                                <select className="outline-none text-gray-50 py-3 px-2 bg-gray-700 rounded-lg w-2/3" id="difficulty" name="difficulty" value={activity.difficulty} onChange={handleInputChange}>
-                                    <option>Select Difficulty..</option>
-                                    {Object.keys(difficulties).map((difficulty) => (
-                                        <option key={difficulty} value={difficulty}>
-                                            {difficulty}
-                                        </option>
-                                    ))}
-                                </select>
-                            </div>
-                            <div className="flex justify-between w-full items-center px-5">
-                                <label className="text-md text-gray-50">Duration</label>
-                                <input type="number" name="duration" value={activity.duration} onChange={handleInputChange} className="appearance-none mb-5 bg-gray-700 rounded-lg border-none w-2/3 text-gray-50 py-3 px-2 leading-tight focus:outline-none" />
-                            </div>
-                            <div className="bg-gray-700 h-[1px]"></div>
-                            <div className="flex justify-end px-5">
-                                <button className="flex-shrink-0 mt-5 bg-secondaryColor/80 hover:bg-secondaryColor font-semibold text-md text-white py-2 px-4 rounded-xl">
-                                    Add Activity
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+  return (
+    <div>
+      <Sidebar></Sidebar>
+      <div className="p-2 sm:p-4 sm:ml-64 h-full bg-panelBg">
+        <div className="flex flex-col w-full items-center border-gray-200 h-full dark:border-gray-700">
+          <Breadcrumb homeElement={"Home"}></Breadcrumb>
+          <div
+            className="rounded-lg shadow-lg h-96 w-full lg:w-2/3 mb-4"
+            id="map"
+          ></div>
+          <div className="flex flex-col shadow-lg items-center justify-center w-full lg:w-2/3 h-full mb-4 rounded-lg bg-gray-800">
+            <div className="flex justify-start h-16 items-center border-b border-gray-700 w-full px-5">
+              <h2 className="text-gray-50 font-bold text-xl">Add Activity</h2>
             </div>
-            <Footer></Footer>
+            <form
+              onSubmit={handleAddActivity}
+              className="w-full space-y-2 py-5"
+            >
+              <div className="flex justify-between w-full items-center px-5">
+                <label className="text-md text-gray-50">Name</label>
+                <input
+                  name="name"
+                  value={activity.name}
+                  onChange={handleInputChange}
+                  className="appearance-none rounded-lg bg-gray-700 border-none w-2/3 text-gray-50 py-3 px-2 leading-tight focus:outline-none"
+                  type="text"
+                  placeholder="Activity name"
+                />
+              </div>
+              <div className="flex justify-between w-full items-center px-5">
+                <label className="text-md text-gray-50">Description</label>
+                <input
+                  name="description"
+                  value={activity.description}
+                  onChange={handleInputChange}
+                  className="appearance-none rounded-lg bg-gray-700 border-none w-2/3 text-gray-50 py-3 px-2 leading-tight focus:outline-none"
+                  type="text"
+                  placeholder="Activity description"
+                />
+              </div>
+              <div className="flex justify-between w-full items-center px-5">
+                <label className="text-md text-gray-50">Category</label>
+                <select
+                  className="outline-none text-gray-50 py-3 px-2 bg-gray-700 rounded-lg w-2/3"
+                  id="category"
+                  name="category"
+                  value={activity.category}
+                  onChange={handleInputChange}
+                >
+                  <option>Select Category..</option>
+                  {interests.map((interest) => (
+                    <option key={interest} value={interest}>
+                      {interest}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="flex justify-between w-full items-center px-5">
+                <label className="text-md text-gray-50">Country</label>
+                <input
+                  value={activity.country}
+                  disabled
+                  className="appearance-none bg-gray-700 border-none rounded-lg w-2/3 text-gray-50 py-3 px-2 leading-tight focus:outline-none"
+                  type="text"
+                  placeholder="Country"
+                />
+              </div>
+              <div className="flex justify-between w-full items-center px-5">
+                <label className="text-md text-gray-50">City</label>
+                <input
+                  value={activity.city}
+                  disabled
+                  className="appearance-none bg-gray-700 border-none w-2/3 rounded-lg text-gray-50 px-2 py-3 leading-tight focus:outline-none"
+                  type="text"
+                  placeholder="City"
+                />
+              </div>
+              <div className="flex justify-between w-full items-center px-5">
+                <label className="text-md text-gray-50">Postal Code</label>
+                <input
+                  value={activity.postalCode}
+                  disabled
+                  className="appearance-none bg-gray-700 border-none w-2/3 rounded-lg text-gray-50 py-3 px-2 leading-tight focus:outline-none"
+                  type="text"
+                  placeholder="Postal Code"
+                />
+              </div>
+              <div className="flex justify-between w-full items-center px-5">
+                <label className="text-md text-gray-50">Street</label>
+                <input
+                  value={activity.address}
+                  disabled
+                  className="appearance-none bg-gray-700 border-none w-2/3 rounded-lg text-gray-50 py-3 px-2 leading-tight focus:outline-none"
+                  type="text"
+                  placeholder="Street"
+                />
+              </div>
+              <div className="flex justify-between w-full items-center px-5">
+                <label className="text-md text-gray-50">Difficulty</label>
+                <select
+                  className="outline-none text-gray-50 py-3 px-2 bg-gray-700 rounded-lg w-2/3"
+                  id="difficulty"
+                  name="difficulty"
+                  value={activity.difficulty}
+                  onChange={handleInputChange}
+                >
+                  <option>Select Difficulty..</option>
+                  {Object.keys(difficulties).map((difficulty) => (
+                    <option key={difficulty} value={difficulty}>
+                      {difficulty}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="flex justify-between w-full items-center px-5">
+                <label className="text-md text-gray-50">Duration</label>
+                <input
+                  type="number"
+                  name="duration"
+                  value={activity.duration}
+                  onChange={handleInputChange}
+                  className="appearance-none mb-5 bg-gray-700 rounded-lg border-none w-2/3 text-gray-50 py-3 px-2 leading-tight focus:outline-none"
+                />
+              </div>
+              <div className="bg-gray-700 h-[1px]"></div>
+              <div className="flex justify-end px-5">
+                <button className="flex-shrink-0 mt-5 bg-secondaryColor/80 hover:bg-secondaryColor font-semibold text-md text-white py-2 px-4 rounded-xl">
+                  Add Activity
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 }
 export default IsAuth(AddActivity);
--- client/src/app/[locale]/panel/dashboard/page.tsx
+++ client/src/app/[locale]/panel/dashboard/page.tsx
@@ -1,29 +1,28 @@
-"use client"
+"use client";
 
-import Footer from '@/components/panel/layout/footer/Footer';
-import Sidebar from '@/components/panel/layout/sidebar/Sidebar';
-import IsAuth from '@/hooks/isAuth';
-import { NextPage } from 'next';
+import IsAuth from "@/hooks/isAuth";
+import { NextPage } from "next";
 import Breadcrumb from "@/components/panel/layout/breadcrumb/Breadcrumb";
-import Map from '@/components/map/map';
-import VenturesNumber from '@/components/panel/dashboardNumbers/DashboardNumbers';
+import Map from "@/components/map/map";
+import VenturesNumber from "@/components/panel/dashboardNumbers/DashboardNumbers";
+import Sidebar from "@/components/panel/layout/sidebar/sidebar";
+import Footer from "@/components/panel/layout/footer/footer";
 
 const Dashboard: NextPage = () => {
-
-    return (
-        <div className="h-[100vh] bg-panelBg">
-            <Sidebar></Sidebar>
-            <div className="sm:ml-64 p-4 h-full">
-            <Breadcrumb homeElement={'Home'}></Breadcrumb>
-                <div className="space-y-4 border-gray-200 h-full dark:border-gray-700">
-                    <VenturesNumber></VenturesNumber>
-                    <div className="h-3/4 mb-10">
-                        <Map></Map>
-                    </div>
-                </div>
-            </div>
-            <Footer></Footer>
+  return (
+    <div className="h-[100vh] bg-panelBg">
+      <Sidebar></Sidebar>
+      <div className="sm:ml-64 p-4 h-full">
+        <Breadcrumb homeElement={"Home"}></Breadcrumb>
+        <div className="space-y-4 border-gray-200 h-full dark:border-gray-700">
+          <VenturesNumber></VenturesNumber>
+          <div className="h-3/4 mb-10">
+            <Map></Map>
+          </div>
         </div>
-    )
-}
-export default IsAuth(Dashboard)
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+export default IsAuth(Dashboard);
