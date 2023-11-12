3a795e173fe061a4ad29b95f7b5cba6da1696db0
Almir Hodžić <81625008+hdzcalmir@users.noreply.github.com>
2023-11-12 20:31:40 +0100

    Responsive fix on add event

--- client/src/app/[locale]/panel/add-event/page.tsx
+++ client/src/app/[locale]/panel/add-event/page.tsx
@@ -149,10 +149,10 @@ function AddEvent() {
                   placeholder="Street"
                 />
               </div>
-              <div className="flex justify-between w-full space-x-5 items-center px-5">
+              <div className="flex flex-col lg:flex-row justify-between w-full lg:space-x-5 items-center px-5">
                 <label className="text-md text-gray-50 w-full">Starting Date</label>
                 <div className="relative w-full flex justify-end">
-                  <div className="flex absolute inset-y-0 left-20 items-center pl-3 pointer-events-none">
+                  <div className="flex absolute inset-y-0 left-0 lg:left-20 items-center pl-3 pointer-events-none">
                     <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                       <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path>
                     </svg>
@@ -165,13 +165,13 @@ function AddEvent() {
                         start_date: date.toISOString()
                       });
                     }}
-                    className="bg-gray-50 border w-2/3 cursor-pointer border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-secondaryColor focus:border-secondaryColor block pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondaryColor dark:focus:border-secondaryColor font-medium"
+                    className="bg-gray-50 border w-full lg:w-2/3 cursor-pointer border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-secondaryColor focus:border-secondaryColor block pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondaryColor dark:focus:border-secondaryColor font-medium"
                     placeholder="DD/MM/YYYY"
                   />
                 </div>
                 <label className="text-md text-gray-50 w-full">Ending Date</label>
                 <div className="relative w-full flex justify-end">
-                  <div className="flex absolute inset-y-0 left-20 items-center pl-3 pointer-events-none">
+                  <div className="flex absolute inset-y-0 left-0 lg:left-20 items-center pl-3 pointer-events-none">
                     <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                       <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path>
                     </svg>
@@ -184,7 +184,7 @@ function AddEvent() {
                         end_date: date.toISOString()
                       });
                     }}
-                    className="bg-gray-50 border cursor-pointer w-2/3 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-secondaryColor focus:border-secondaryColor block pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondaryColor dark:focus:border-secondaryColor font-medium"
+                    className="bg-gray-50 border cursor-pointer w-full lg:w-2/3 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-secondaryColor focus:border-secondaryColor block pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondaryColor dark:focus:border-secondaryColor font-medium"
                     placeholder="DD/MM/YYYY"
                   />
                 </div>
