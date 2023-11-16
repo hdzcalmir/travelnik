aededfc5e88317538ec368b324bd88d4b170f919
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2023-11-16 10:55:11 +0100

    Update EventCalendar.tsx

--- client/src/components/home/departure/EventCalendar/EventCalendar.tsx
+++ client/src/components/home/departure/EventCalendar/EventCalendar.tsx
@@ -15,46 +15,58 @@ const EventCalendar = ({ startDate, endDate, events }: EventCalendarProps) => {
   const defaultValues = startDate && endDate ? [startDate, endDate] : undefined;
 
   const tileContent = ({ date }: { date: Date }) => {
-    const eventDates =
-      events && events?.map((event) => new Date(event.start_date));
-    const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1
-      }-${date.getDate()}`;
+    const isEventDate = events?.some((event) => {
+      const eventStartDate = new Date(event.start_date);
+      const eventEndDate = new Date(event.end_date);
 
-    const isEventDate =
-      events &&
-      eventDates?.some((eventDate) => {
-        const formattedEventDate = `${eventDate.getFullYear()}-${eventDate.getMonth() + 1
-          }-${eventDate.getDate()}`;
-        return formattedEventDate === formattedDate;
-      });
+      // Add one day to start and end dates
+      eventStartDate.setDate(eventStartDate.getDate());
+      eventEndDate.setDate(eventEndDate.getDate() + 1);
+
+      return date >= eventStartDate && date <= eventEndDate;
+    });
 
     if (isEventDate) {
       const event = events?.find((event) => {
-        const eventDate = new Date(event.start_date);
-        const formattedEventDate = `${eventDate.getFullYear()}-${eventDate.getMonth() + 1
-          }-${eventDate.getDate()}`;
-        return formattedEventDate === formattedDate;
+        const eventStartDate = new Date(event.start_date);
+        const eventEndDate = new Date(event.end_date);
+
+        // Add one day to start and end dates
+        eventStartDate.setDate(eventStartDate.getDate());
+        eventEndDate.setDate(eventEndDate.getDate() + 1);
+
+        return date >= eventStartDate && date <= eventEndDate;
       });
 
-      return (
-        <Tooltip
-          content={
-            <div className="px-1 py-2">
-              <div className="text-small font-bold">{event?.name}</div>
-              <div className="text-tiny">Category: {event?.category}</div>
-              <div className="text-tiny">Description: {event?.description}</div>
-              <div className="text-tiny">
-                End date:{" "}
-                {event?.end_date
-                  ? new Date(event.end_date).toLocaleDateString("en-GB")
-                  : ""}
+      if (event) {
+        // Add one more day to the end date for display
+        const displayEndDate = new Date(event.end_date);
+        displayEndDate.setDate(displayEndDate.getDate() + 1);
+
+        return (
+          <Tooltip
+            content={
+              <div className="px-1 py-2">
+                <div className="text-small font-bold">{event.name}</div>
+                <div className="text-tiny">Category: {event.category}</div>
+                <div className="text-tiny">
+                  Description: {event.description}
+                </div>
+                <div className="text-tiny">
+                  End date:{" "}
+                  {event.end_date
+                    ? displayEndDate.toLocaleDateString("en-GB", {
+                        timeZone: "UTC",
+                      })
+                    : ""}
+                </div>
               </div>
-            </div>
-          }
-        >
-          <div className="event-tile h-1.5 w-1.5 rounded-full bg-white/70 mx-auto"></div>
-        </Tooltip>
-      );
+            }
+          >
+            <div className="event-tile h-1.5 w-1.5 rounded-full bg-white/70 mx-auto"></div>
+          </Tooltip>
+        );
+      }
     }
 
     return null;
