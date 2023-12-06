1bd3e90849296b8376ea3ded84813e84301944fc
Ibrahim Okić <ibrahimokic711@gmail.com>
2023-12-06 12:01:02 +0100

    Update Chat.tsx

--- client/src/components/home/chat/Chat.tsx
+++ client/src/components/home/chat/Chat.tsx
@@ -32,7 +32,7 @@ const Chat = ({ chatOpened, setChatOpened }: ChatProps) => {
                     </div>
                     <button className="text-white hover:text-gray-200 transition duration-500" onClick={() => setChatOpened(!chatOpened)}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
                         </svg>
                     </button>
                 </div>
