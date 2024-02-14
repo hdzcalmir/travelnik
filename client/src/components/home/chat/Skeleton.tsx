f25ad1cc4181a659abd6642f583e58b87f55fa2e
Ibrahim Okić <ibrahimokic711@gmail.com>
2024-02-14 16:01:18 +0100

    Update Skeleton.tsx

--- client/src/components/home/chat/Skeleton.tsx
+++ client/src/components/home/chat/Skeleton.tsx
@@ -6,7 +6,7 @@ const MessageSkeleton = () => {
                 <p className="bg-gray-200 text-gray-700 rounded-lg py-2 px-4 inline-block animate-pulse w-60"></p>
             </div>
             <div className="mb-2 text-right">
-                <p className="bg-secondaryColor text-white rounded-lg py-2 px-4 inline-block w-60"></p>
+                <p className="bg-secondaryColor text-white rounded-lg py-2 px-4 inline-block animate-pulse w-60"></p>
             </div>
         </>
     );
